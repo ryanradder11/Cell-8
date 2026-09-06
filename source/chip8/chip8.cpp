3b43c37da718882ef94b6060bc0c764a3d47993e
ryanradder <ryan.radder@headfirst.nl>
2026-09-06 10:51:22 +0200

    rename and remove print comments

--- source/chip8/chip8.cpp
+++ source/chip8/chip8.cpp
@@ -223,19 +223,11 @@ void emulateCycle(Chip8 &chip)
 				break;
 				//Set VX equal to VX bitshifted right 1. VF is set to the least significant bit of VX prior to the shift.
 				case 0x6: {
-					uint8_t x_value = chip.V[x];
-					// printf("[8xy6 pre]  x=%d y=%d\n", x, y);
-					// printBinary8("[8xy6] V[x] before AND", chip.V[x]);
-					// printBinary8("[8xy6] mask 0x1        ", 0x1);
+					uint8_t vx = chip.V[x];
 
+					chip.V[x] = vx  >> 1;
+					chip.V[0xF] = vx & 0x1;
 
-					// printBinary8("[8xy6] V[x] & 0x1 -> VF", chip.V[0xF]);
-					// printf("[8xy6 mid]  after VF write: V[x]=%d V[0xF]=%d\n", chip.V[x], chip.V[0xF]);
-
-					chip.V[x] = x_value >> 1;
-					chip.V[0xF] = x_value & 0x1;
-
-					// printf("[8xy6 post] V[x]=%d V[0xF]=%d\n", chip.V[x], chip.V[0xF]);
 					chip.pc += 2;
 					if (debug) printf("Shift V[%d] right by 1. VF = %d\n", x, chip.V[0xF]);
 				}
